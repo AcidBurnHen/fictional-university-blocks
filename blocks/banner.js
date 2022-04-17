8f04151dd6f38082dac618a9146215f74cba0874
Marin <cc.acidburn@gmail.com>
2022-04-17 18:47:33 +0200

    Added inner blocks to banner block

--- blocks/banner.js
+++ blocks/banner.js
@@ -1,3 +1,5 @@
+import { InnerBlocks } from "@wordpress/block-editor";
+
 wp.blocks.registerBlockType("fictionalblocktheme/banner", {
   title: "Banner",
   edit: EditComponent,
@@ -5,23 +7,36 @@ wp.blocks.registerBlockType("fictionalblocktheme/banner", {
 });
 
 function EditComponent() {
+  const useLater = (
+    <>
+      <h1 className="headline headline--large">Welcome!</h1>
+      <h2 className="headline headline--medium">We think you&rsquo;ll like it here.</h2>
+      <h3 className="headline headline--small">
+        Why don&rsquo;t you check out the <strong>major</strong> you&rsquo;re interested in?
+      </h3>
+      <a href="#" className="btn btn--large btn--blue">
+        Find Your Major
+      </a>
+    </>
+  );
+
   return (
     <div className="page-banner">
       <div className="page-banner__bg-image" style={{ backgroundImage: "url('/wp-content/themes/fictional-block-theme/images/library-hero.jpg')" }}></div>
       <div className="page-banner__content container t-center c-white">
-        <h1 className="headline headline--large">Welcome!</h1>
-        <h2 className="headline headline--medium">We think you&rsquo;ll like it here.</h2>
-        <h3 className="headline headline--small">
-          Why don&rsquo;t you check out the <strong>major</strong> you&rsquo;re interested in?
-        </h3>
-        <a href="#" className="btn btn--large btn--blue">
-          Find Your Major
-        </a>
+        <InnerBlocks />
       </div>
     </div>
   );
 }
 
 function SaveComponent() {
-  return <p>This is from the block</p>;
+  return (
+    <div className="page-banner">
+      <div className="page-banner__bg-image" style={{ backgroundImage: "url('/wp-content/themes/fictional-block-theme/images/library-hero.jpg')" }}></div>
+      <div className="page-banner__content container t-center c-white">
+        <InnerBlocks.Content />
+      </div>
+    </div>
+  );
 }
--- build/banner.asset.php
+++ build/banner.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-element'), 'version' => '90e9db6d36862012ae7128f80110f547');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-element'), 'version' => 'a067575fdb4c7d7de5a8bcdc16b38a39');
